2f3bcffb7e7f7acf9e2d0527beccb7263249795d
Daniel Ntege <danientege785@gmail.com>
2024-11-22 15:28:51 +0300

    Initial commit

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,13 @@
+export const appwriteConfig = {
+  endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
+  databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
+  usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
+  filesCollectionId: process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
+  approverCollectionId: process.env.NEXT_PUBLIC_APPWRITE_APPROVER_COLLECTION!,
+  publicationCollectionId: process.env.NEXT_PUBLIC_APPWRITE_PUBLICATION_COLLECTION!,
+  departmentCollectionId: process.env.NEXT_PUBLIC_APPWRITE_DEPARTMENT_COLLECTION!,
+  reviewCollectionId: process.env.NEXT_PUBLIC_APPWRITE_REVIEW_COLLECTION!,
+  bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
+  secretKey: process.env.NEXT_APPWRITE_SECRET!,
+};
