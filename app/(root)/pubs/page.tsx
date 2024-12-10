1819a58accb1d33cbc647cb25439b8097ad76dc5
Daniel Ntege <danientege785@gmail.com>
2024-12-10 21:31:27 +0300

    commit

---
+++ app/(root)/pubs/page.tsx
@@ -0,0 +1,9 @@
+import { PublicationBrowser } from "./_components/pubs-browser";
+
+export default function FilesPage() {
+  return (
+    <div>
+      <PublicationBrowser title="All Your Files" />
+    </div>
+  );
+}
