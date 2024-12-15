f15049dd634a3eb44436a956486bcdab83eb1873
TroyMoses <mugabimoses07@gmail.com>
2024-12-15 17:08:21 +0300

    prototype setup

---
+++ app/(home)/my-profile/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MyProfilePage = async () => {
+
+  return (
+    <div className="w-full px-5">
+      <h2 className="text-xl font-semibold">My Profile Page</h2>
+      <p>This is the my profile page content.</p>
+    </div>
+  );
+};
+
+export default MyProfilePage;
