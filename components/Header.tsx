274f228f45f071679e1a5f0e973caae45d9a64a8
TroyMoses <mugabimoses07@gmail.com>
2024-12-15 17:08:29 +0300

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,10 +9,10 @@ import { Input } from '@/components/ui/input';
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center justify-between space-x-10">
+      <div className="container flex h-14 items-center justify-between space-x-8">
         <div className="mr-4 flex items-center space-x-2">
           <Link href="/home" className="flex items-center space-x-2">
-            <span className="text-xl font-bold">BU Scholar</span>
+            <span className="text-xl font-bold">BU Publications</span>
           </Link>
         </div>
         <div className="flex flex-1 items-center space-x-2">
